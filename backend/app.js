8083a3dd67f221ee80499f155e790fa5ba291a26
ibrahim <ibrahim1001.sc@gmail.com>
2022-10-01 10:54:10 +0500

    error handler added

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const goalsRoute = require("./routes/goals/goals.route");
+const {errorHandler} = require('./middlerware/errorMiddleware')
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-app.get("/", (req, res) => {
-  res.send("Hello from express");
-});
+
 app.use("/api/goals", goalsRoute);
+app.use(errorHandler);
 
 module.exports = app;
