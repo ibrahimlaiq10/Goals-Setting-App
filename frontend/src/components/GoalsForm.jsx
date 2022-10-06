45891641bd0ca4f3a9aa1a10007174e983664926
ibrahim <ibrahim1001.sc@gmail.com>
2022-10-07 00:29:19 +0500

    goals crud and frontend build

--- frontend/src/components/GoalsForm.jsx
+++ frontend/src/components/GoalsForm.jsx
@@ -1,8 +1,40 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { createGoal } from '../features/Goals/goalSlice'
 
-function GoalsForm() {
+function GoalForm() {
+  const [text, setText] = useState('')
+
+  const dispatch = useDispatch()
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    dispatch(createGoal({ text }))
+    setText('')
+  }
+
   return (
-    <div>GoalsForm</div>
+    <section className='form'>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='text'>Goal</label>
+          <input
+            type='text'
+            name='text'
+            id='text'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div className='form-group'>
+          <button className='btn btn-block' type='submit'>
+            Add Goal
+          </button>
+        </div>
+      </form>
+    </section>
   )
 }
 
-export default GoalsForm
+export default GoalForm
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,24 +1,56 @@
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import GoalsForm from "../components/GoalsForm";
+import { getGoals, reset } from "../features/Goals/goalSlice";
+import Spinner from "../components/Spinner";
+import GoalItem from '../components/GoalItem'
 
 function Dashboard() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goal
+  );
 
   useEffect(() => {
+    if (isError) {
+      console.log(message);
+    }
+
     if (!user) {
       navigate("/login");
     }
-  }, [user, navigate]);
 
+    dispatch(getGoals());
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, isError, message, dispatch]);
+  if (isLoading) {
+    return <Spinner />;
+  }
   return (
-    <section className="heading">
-      <h1>Welcome {user?.name}</h1>
-      <GoalsForm />
-    </section>
+    <>
+      <section className="heading">
+        <h1>Welcome {user?.name}</h1>
+        <GoalsForm />
+      </section>
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
+    </>
   );
 }
 
